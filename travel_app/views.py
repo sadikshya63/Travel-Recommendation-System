44a1732cd4fd2db05486aeaa83437a52c80aa072
Sarita Puri <saritaapuri20@gmail.com>
2026-06-30 10:14:49 +0545

    form

--- travel_app/views.py
+++ travel_app/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def recommendation(request):
+    return render(request, 'recommendation.html')
--- travel_project/urls.py
+++ travel_project/urls.py
@@ -22,4 +22,5 @@ from travel_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+     path('recommendation/', views.recommendation, name='recommendation'),
 ]
