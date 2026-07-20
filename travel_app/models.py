dcf87bec3175d6b6390f09a71a17db750a37e417
DristiBhattarai <dristi2061@gmail.com>
2026-07-20 19:48:46 +0545

    add

--- travel_app/models.py
+++ travel_app/models.py
@@ -32,7 +32,7 @@ class Hotel(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='hotels')
     hotel_name = models.CharField(max_length=150)
     address = models.CharField(max_length=255, blank=True, null=True)
-    price_range = models.CharField(max_length=100)   # Low / Medium / High
+    price_range = models.CharField(max_length=100)   
     rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
     contact_number = models.CharField(max_length=20, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
